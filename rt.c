fd048e38e3c82cb86457950f834c235d396d5495
Andrei Boghean <andreiboghean00@gmail.com>
2023-04-07 18:54:14 +0100

    complete lab 2

--- rt.c
+++ rt.c
@@ -7,23 +7,51 @@
 
 #include <stdio.h>
 
+#define N 30
+#define M 30
+
 int main(void) {
+	int horizontalRayCount=N, verticalRayCount=M;
+
 	double cx, cy, cz, r2; //x,y,z components of sphere centre, radius squared
-	cx = 1.0 ; cy = 1.0; cz = 2.0; r2 = 1.5; //change here to move sphere around
-	double ux, uy, uz, vx, vy, vz; //origin of line, unit vector direction 
-	ux = uy = uz = 0; //shoot ray from the actual origin to start with
-	vx = vy = 0; vz = 1.0; //a ray along the z direction
-
-	double Dx = ux - cx, Dy = uy - cy, Dz = uz - cz;
-	double D2 = Dx*Dx + Dy*Dy + Dz*Dz;
-	double vD = vx*Dx + vy*Dy + vz*Dz ;
-	double disc = vD*vD - D2 + r2;
+	printf("please enter the x, y, and z value of the sphere in format x,y,z\n");
+	scanf("%lf,%lf,%lf", &cx, &cy, &cz);
+	printf("please enter radius of the sphere\n");
+	scanf("%lf", &r2);
+	r2 = r2*r2; // we store r^2 but the user gave us r
 
 
-	if (disc < 0) //discriminant is negative so no intersection
-		{ printf(" "); }
-	else //grazing or full intersection
-		{ printf("X"); }
+	double ux, uy, uz, vx, vy, vz; //origin of line, unit vector direction
+
+	uz = 0; // plane from which the orthographic projection starts from does not vary in z,
+			// only in x and y, so we can keep z constantly set to 0 from the begining.
+
+	for (int j=0; j<verticalRayCount ; j++ )
+	{
+		uy = (2 * j/((double)verticalRayCount)) - 1;
+		// ^ scale any number of rays down to a range from -1 to 1 (exclusive (since we want the side of our "pixels" to be touching the side of the viewport, rather than the pixels being centered on the side of the viewport.))
+
+		for (int i=0 ; i<horizontalRayCount ; i++ )
+		{
+			ux = (2 * i/((double)horizontalRayCount)) - 1;
+			// ^ scale any number of rays down to a range from -1 to 1 (exclusive (since we want the side of our "pixels" to be touching the side of the viewport, rather than the pixels being centered on the side of the viewport.))
+
+			vx = vy = 0; vz = 1.0; //a ray along the z direction
+
+			double Dx = ux - cx, Dy = uy - cy, Dz = uz - cz;
+			double D2 = Dx*Dx + Dy*Dy + Dz*Dz;
+			double vD = vx*Dx + vy*Dy + vz*Dz ;
+			double disc = vD*vD - D2 + r2;
+
+
+			if (disc < 0) //discriminant is negative so no intersection
+				{ printf(" "); }
+			else //grazing or full intersection
+				{ printf("X"); }
+		}
+		printf("\n");
+	}
+
 
 	return 0;
 }
